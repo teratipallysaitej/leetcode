b9b2f5a22ddf524be44caee3278e1714b8808c63
teratipallysaitej <47312773+teratipallysaitej@users.noreply.github.com>
2023-08-18 15:09:40 -0700

    Time: 82 ms (91.94%), Space: 49.7 MB (67.74%) - LeetHub

--- 1738-maximal-network-rank/1738-maximal-network-rank.js
+++ 1738-maximal-network-rank/1738-maximal-network-rank.js
@@ -1,40 +1,30 @@
-/**
- * @param {number} n
- * @param {number[][]} roads
- * @return {number}
- */
 var maximalNetworkRank = function(n, roads) {
-  if(roads.length == 0){
-    return 0
-  }
-    mp = new Map()
-    st = new Map()
-    for(let i = 0; i<roads.length;i++){
-      if(mp[roads[i][0]]){
-        mp[roads[i][0]] += 1
-      }
-      else {
-        mp[roads[i][0]] = 1
-      }
-      if(mp[roads[i][1]]){
-        mp[roads[i][1]] += 1
-      }
-      else {
-        mp[roads[i][1]] = 1
-      }
-      st[JSON.stringify([roads[i][0],roads[i][1]])] = 1
-    }
-    let mx = 0
-    for(let i = 0;i<n-1;i++){
-      for(let j = i+1;j<n;j++){
-        if(mp[i] && mp[j]){
-        let val = mp[i] + mp[j]
-        if(st[JSON.stringify([i,j])] || st[JSON.stringify([j,i])]){
-          val -= 1
+    let maxRank = 0;
+    let adj = new Map();
+    // Construct adjency list 'adj', where adj[node] stores all nodes connected to 'node'.
+    for (let road of roads) {
+        if (!adj.has(road[0])) {
+            adj.set(road[0], new Set());
         }
-        mx = Math.max(mx,val)
+        if (!adj.has(road[1])) {
+            adj.set(road[1], new Set());
         }
-      }
+        adj.get(road[0]).add(road[1]);
+        adj.get(road[1]).add(road[0]);
     }
-    return mx
+
+    // Iterate on each possible pair of nodes.
+    for (let node1 = 0; node1 < n; ++node1) {
+        for (let node2 = node1 + 1; node2 < n; ++node2) {
+            let currentRank = (adj.get(node1) || new Set()).size +
+                              (adj.get(node2) || new Set()).size;
+            if ((adj.get(node1) || new Set()).has(node2)) {
+                --currentRank;
+            }
+            // Find the current pair's respective network rank and store if it's maximum till now.
+            maxRank = Math.max(maxRank, currentRank);
+        }
+    }
+    // Return the maximum network rank.
+    return maxRank;
 };
