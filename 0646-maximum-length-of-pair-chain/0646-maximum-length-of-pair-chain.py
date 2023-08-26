76650d1358946edc53715e475eee8bc3e5ac1119
teratipallysaitej <47312773+teratipallysaitej@users.noreply.github.com>
2023-08-26 03:28:07 -0700

    Time: 1590 ms (42.79%), Space: 16.9 MB (35.7%) - LeetHub

---
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs.sort(key = lambda x:(x[0],x[1]))
+        mn = 1
+        dp = [1]*len(pairs)
+        n = len(pairs)
+        for i in range(n-2,-1,-1):
+            for j in range(i+1,n):
+                if pairs[i][1] < pairs[j][0]:
+                    dp[i] = max(dp[i],dp[j]+1)
+        return max(dp)
+
