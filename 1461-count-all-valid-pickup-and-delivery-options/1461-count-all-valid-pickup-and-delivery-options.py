f420283a6f56b871c7957091a7445291b821bc41
teratipallysaitej <47312773+teratipallysaitej@users.noreply.github.com>
2023-09-10 17:52:15 -0700

    Time: 31 ms (94.47%), Space: 16.2 MB (70.21%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.py
@@ -1,12 +1,7 @@
-
+MOD = 10**9 + 7
 class Solution:
     def countOrders(self, n: int) -> int:
-        if n ==1:
-            return 1
-        c = 1
-        e = 10**9+7
-        for i in range(2,n+1):
-            pos = (i-1)*2 + 1
-            c = c*((pos)*(pos+1))//2
-
-        return c%e
+        count = 1 
+        for i in range(2, n + 1):
+            count = (count * (2 * i - 1) * i) % MOD
+        return count
