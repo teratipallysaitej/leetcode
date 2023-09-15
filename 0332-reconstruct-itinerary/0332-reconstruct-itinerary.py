70f05eb5e8a3d6af8acd2c9e101964e46d3ff5e0
teratipallysaitej <47312773+teratipallysaitej@users.noreply.github.com>
2023-09-14 20:05:01 -0700

    Time: 81 ms (87.67%), Space: 17.1 MB (53.25%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        d = defaultdict(list)
+        hel = set()
+        # for a,d1 in tickets:
+        #     hel.add(a)
+        #     hel.add(d1)
+        #     d[a] = []
+        #     d[d1] = []
+        for a, d1 in tickets:
+            d[a].append(d1)
+        for a in d:
+            d[a].sort()
+        res = []
+        # count = len(hel)
+        def helper(node, it):
+            global res
+            # z = count
+            # for key in d:
+            #     if len(d[key]) == 0:
+            #         z -= 1
+
+            if len(it) == len(tickets) + 1:
+                res = it[:]
+                return True
+            if node not in d:
+                return False
+            # if z == 0:
+            #     q = it[:]
+            #     res.append(q)
+            for i,n in enumerate(d[node]):
+                it.append(n)
+                d[node].pop(i)
+                # print(it, n, d)
+                if helper(n,it): return True
+                it.pop(-1)
+                d[node].insert(i,n)
+
+            return False
+        it = []
+        it.append("JFK")
+        helper("JFK", it)
+        # for key in d:
+        #     if len(d[key]):
+        #         res.append(key)
+        #         helper(key)
+        return it
+
